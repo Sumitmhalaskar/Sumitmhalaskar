a6e11cfc23415f50bbd9f0c52a6119c5f53fa325
Sumit mhalaskar <sumit.mhalaskar24@sanjivani.edu.in>
2024-09-06 21:29:24 +0530

    Add files via upload
    You are working on a small project to manage employee information at a company. As part
    of this project, you need to write a C++ program that:
    1. Defines identifiers to store basic details about an employee, such as their name, age, and
    employee ID.
    2. Uses appropriate keywords to declare and initialise these variables.
    3. Includes comments to explain the purpose of each part of the code, making it easier for
    other developers to understand and maintain.

---
+++ Main (11).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+    string emploeename = "john dee";
+    int emploeeage = 34;
+    string emploeeid = " gdg48843:";
+    
+    
+    cout<<"the emploeename is = "<<emploeename<<endl;
+    cout<<"The emploeeage is = "<<emploeeage<<endl;
+    cout<<"the emploeeid is = "<<emploeeid<<endl;
+    return 0;
+}
